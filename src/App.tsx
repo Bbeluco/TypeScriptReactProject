6b35cbed67c44eba051ef440d9cd8d39d6613116
bbeluco <beluco.bispo@hotmail.com>
2023-08-22 21:13:48 -0300

    Funcion component + list created without array

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Formulario from './components/Formulario/formularioComponent';
+import Lista from './components/Lista/ListaComponent';
 
 function App() {
   return (
     <div className="App">
-      
+      <Formulario />
+      <Lista />
     </div>
   );
 }
---
+++ src/components/Botao/buttonComponent.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Botao extends React.Component {
+    render(): React.ReactNode {
+        return <button>BOTAO</button>
+    }
+}
+
+export default Botao;
