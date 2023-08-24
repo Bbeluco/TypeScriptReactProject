a3ce2f69341b01a8cbb4458736978d35f7020b61
bbeluco <beluco.bispo@hotmail.com>
2023-08-24 19:15:12 -0300

    Update ListaComponent based on state

--- src/components/Lista/ListaComponent.tsx
+++ src/components/Lista/ListaComponent.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import Item from './ItemComponent';
 import style from './Lista.module.scss';
+import { useState } from 'react';
 
 function Lista(){
-    const tarefas = [{
+    const [tarefas, setTarefas] = useState([{
         nome: "React",
         tempo: "02:00:00"
     },
     {
         nome: "Typescript",
         tempo: "01:00:00"
-    }]
+    }])
     return (
         <aside className={style.listaTarefas}>
-            <label>Lista de estudos</label>
+            <h2 onClick={() => {
+                setTarefas([...tarefas, {nome: "Estudo de estados", tempo: "05:00:00"}])
+            }}>Lista de estudos</h2>
             <ul>
                 {tarefas.map((item, index) => (
                     <Item key={index} {...item}/>
