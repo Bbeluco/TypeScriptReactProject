ffa9bb6b94bb3579e2931c8dcd314dfd352af368
bbeluco <beluco.bispo@hotmail.com>
2023-08-25 16:26:01 -0300

    tempoParaSegundos conversion created

--- src/components/Cronometro/CronometroComponent.tsx
+++ src/components/Cronometro/CronometroComponent.tsx
@@ -1,8 +1,10 @@
 import Botao from "../Botao/buttonComponent";
 import Relogio from "./Relogio/RelogioComponent";
 import style from './Cronometro.module.scss';
+import { tempoParaSegundos } from "../../commons/utils/date";
 
 function Cronometro(){
+    console.log("Tempo convertido: " + tempoParaSegundos('01:01:01'));
     return (
         <div className={style.cronometro}>
             <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
